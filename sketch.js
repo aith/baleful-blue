6b7601fc13d4a4db1e8c11a67132bd4ac26a16ee
aith <ariiramanesh@gmail.com>
2021-02-20 11:46:00 -0800

    drew circle

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+/* todo
+ * - draw variable circle
+ * - subdivide with skewing out
+ * - layer cirle's iterations of 3 subd's with diminishing opacity
+ * - play with color equation
+ */
+
 let can; let canw = 800; let canh = 800;
 
 function setup() {
@@ -6,4 +13,17 @@ function setup() {
 }
 
 function draw() {
+    drawCircle(7)
+    noLoop()
+}
+
+function drawCircle(verts) {
+    let off = 200
+    let r = 100
+    translate(off, off)
+    for(let i = -Math.PI; i < Math.PI; i+=(2*Math.PI)/verts) {
+        ellipse(cos(i) * r, sin(i) * r, 10, 10)
+        print(1)
+    }
+    translate(-off, -off)
 }
